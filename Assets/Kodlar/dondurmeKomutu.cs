ae312b763744f26574258f820cd82dd842dc1daa
Gökhan Çavuş <gokhan.8534b@gmail.com>
2018-02-23 21:59:51 +0300

    bolum_1
    İlk çalışmamız. (REZalet)

---
+++ Assets/Kodlar/dondurmeKomutu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dondurmeKomutu : MonoBehaviour
+{
+
+    public int donusHiziX, donusHiziY,donusHiziZ;
+    byte tekrar;
+	// Use this for initialization
+	void Start ()
+    {
+        tekrar = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(donusHiziX * Time.deltaTime, donusHiziY * Time.deltaTime , donusHiziZ * Time.deltaTime);
+
+
+        //tekrar++;
+        //if (tekrar < 125)
+        //    transform.Rotate(donusHiziX * Time.deltaTime, donusHiziY * Time.deltaTime , donusHiziZ * Time.deltaTime);
+        //else
+        //    transform.Rotate(donusHiziX * Time.deltaTime, -donusHiziY * Time.deltaTime, donusHiziZ * Time.deltaTime);
+
+        //if (tekrar > 250)
+        //    tekrar = 0;
+    }
+}
---
+++ Assets/Kodlar/ileriGeriKomutu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ileriGeriKomutu : MonoBehaviour
+{
+    public int posizyonX, posizyonY, posizyonZ;
+    int tekrar;
+
+    // Use this for initialization
+    void Start ()
+    {
+        tekrar = 0;
+    }
+
+
+
+    bool yon = false;
+	// Update is called once per frame
+	void Update ()
+    {
+        //if(transform.position.z > 86.73f)
+        //{
+        //    yon = true;
+        //}
+        //else if(transform.position.z < 58.23)
+        //{
+        //    yon = false;
+        //}
+
+        //tekrar++;
+        //if (transform.position.z < 86.73f && yon == false)
+        //{
+        //    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + (tekrar * 0.01f));
+
+        //}
+        //else if (transform.position.z > 58.23f && yon == true)
+        //{
+        //    tekrar = 0;
+        //    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - (tekrar * 0.01f));
+        //}
+
+
+        tekrar++;
+
+        if (tekrar > 160)
+            tekrar = 0;
+
+        if (tekrar < 80)
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + (tekrar * 0.01f));
+        else
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - (tekrar * 0.01f));
+
+        
+    }
+}
